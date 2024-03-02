7ba44147884be394f1082012821e58ef8f75adf7
Clara Teodósio <134892305+ClaraTeoMorais@users.noreply.github.com>
2024-03-02 12:06:48 +0000

    questao 4

--- Aula_03-01.03/Lista_3/q4.py
+++ Aula_03-01.03/Lista_3/q4.py
@@ -1,2 +1,14 @@
-b,a = input("Digite a base e a altura do retângulo: ").split
-print (b)
+import math
+
+base,altura = input("Digite a base e a altura do retângulo (respectivamente): ").split()
+
+b = float(base)
+h = float(altura)
+
+a = b*h
+p = (2*b) + (2*h)
+d = math.sqrt((b**2) + (h**2))
+
+print(f"Área = {a:.2f}")
+print(f"Perímetro = {p:.2f}")
+print(f"Diagonal = {d:.2f}")
