4fd3583a6ed767f43c105d21cc23fcd86e01939a
Clara Teodósio <134892305+ClaraTeoMorais@users.noreply.github.com>
2024-03-06 14:56:57 +0000

    bhaskara

--- Aula_04-05.03/1036-Fórmula_de_Bhaskara.py
+++ Aula_04-05.03/1036-Fórmula_de_Bhaskara.py
@@ -16,17 +16,10 @@ else:
     r1 = (-b + math.sqrt(delta)) / (2 * a)
     r2 = (-b - math.sqrt(delta)) / (2 * a)
 
+    print (f"R1 = {r1:.5f}")
+    print (f"R2 = {r2:.5f}")
 
-print (f"R1 = {r1:.5f}")
-print (f"R2 = {r2:.5f}")
 
-# escreva("(-b + ou - raiz(delta)) / (2 * a)\n")
-# escreva("(-", b," + ou - raiz(", delta,")) / (2 * ", a,")\n")
-# escreva("(", -1 * b," + ou - ", Mat.raiz(delta, 2),") / ", 2 * a,"\n")
 
-# escreva("x1 = ", (-1 * b + Mat.raiz(delta, 2)) / (2 * a),"\n")
 
-# escreva("x2 = ", (-1 * b - Mat.raiz(delta, 2)) / (2 * a),"\n")
-# }
-# }
 
