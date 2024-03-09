2b2607432214047fb255fa92175dceea231e2805
Clara Teodósio <134892305+ClaraTeoMorais@users.noreply.github.com>
2024-03-09 15:59:05 +0000

    questao d

--- Aula_04-05.03/d-1040-Distância_Entre_Dois_Pontos.py
+++ Aula_04-05.03/d-1040-Distância_Entre_Dois_Pontos.py
@@ -1,11 +1,14 @@
+import math
+
 x1, y1 = input().split()
 x2, y2 = input().split()
 
 X1 = float(x1)
-Y1 = float(y1)
 X2 = float(x2)
+Y1 = float(y1)
 Y2 = float(y2)
 
-Distancia = ((X2 - X1)**2 + (Y2 - Y1)**2) * 0.5
+distancia = math.sqrt( ((X2 - X1)**2) + ((Y2 - Y1)**2) )
 
-print(f'{Distancia:.4f}')
+print(f"{distancia:.4f}")
+
