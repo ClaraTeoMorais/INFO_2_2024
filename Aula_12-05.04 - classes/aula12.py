bbeef6ccd62a0f32962952c47bddcbad22cf39d6
Clara Teodósio <134892305+ClaraTeoMorais@users.noreply.github.com>
2024-04-05 18:56:04 +0000

    aula 12

---
+++ Aula_12-05.04 - classes/aula12.py
@@ -0,0 +1,9 @@
+#classe -> modelo de uma entidade
+class Circulo:  #modelo oo (orientado a obj.) de uma figura geométrica: círculo
+    pass
+
+a = Circulo()  #a/b = referência -> guarda o local de memória do objeto / endereço -> variável
+b = Circulo()  #"Circulo()" = objeto -> algo criado com o modelo -> variável
+
+# lista -> coleção de coisas q acessa com a posição 
+# dicionário -> coleção de coisas que se acessa com uma chave
