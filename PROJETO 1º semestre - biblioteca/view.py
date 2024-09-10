58c894df677de1d3eff1e623bcc23be542399210
Clara Teodósio <134892305+ClaraTeoMorais@users.noreply.github.com>
2024-09-10 19:04:56 +0000

    trabalho PEOO

---
+++ PROJETO 1º semestre - biblioteca/view.py
@@ -0,0 +1,25 @@
+from exemplar import Exemplar, Exemplares
+from genero import Genero, Generos
+from livro import Livro, Livros
+
+def cliente_inserir(nome, email, fone):
+    c = Cliente(0, nome, email, fone)
+    Clientes.inserir(c)
+
+def horario_inserir(data):
+    c = Horario(0, data)
+    Horarios.inserir(c)
+
+def cliente_listar():
+    return Clientes.listar()    
+
+def horario_listar():
+    return Horarios.listar()    
+
+def cliente_atualizar(id, nome, email, fone):
+    c = Cliente(id, nome, email, fone)
+    Clientes.atualizar(c)
+
+def cliente_excluir(id):
+    c = Cliente(id, "", "", "")
+    Clientes.excluir(c)    
