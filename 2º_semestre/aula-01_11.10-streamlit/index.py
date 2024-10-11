dd282d5c70674c9322acd6a7a0d2fc246d2d62dd
Clara Teodósio <134892305+ClaraTeoMorais@users.noreply.github.com>
2024-10-11 18:57:10 +0000

    aula 11/10

---
+++ 2º_semestre/aula-01_11.10-streamlit/index.py
@@ -0,0 +1,7 @@
+import streamlit as st
+# como o nome da biblioteca é um pouco grande, usa-se um apelido "st", e para renomear e passar a usar esse apelido, usa-se o "as"
+
+st.header("POO em Python com StramLit")
+
+if st.button("Clique aqui!"):
+    st.write("Bem-vindo(a) ao StreamLit") 
